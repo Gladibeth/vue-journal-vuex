42658b64498caa9b2c1f76d6dd1e8faa53cbe3e4
Gladibeth <gladibethdelosa@gmail.com>
2023-07-07 11:36:55 -0500

    Add condition load entries when isloding change

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -2,7 +2,16 @@
   <div>
     <Navbar/>
 
-    <div class="d-flex">
+    <div v-if="isLoading" class="row justify-content-md-center">
+      <div class="col-3 alert-info text-center mt-5">
+        Espere por favor...
+        <h3 class="mt-2">
+          <i class="fa fa-spin fa-sync"></i>
+        </h3>
+      </div>
+    </div>
+
+    <div v-else class="d-flex">
       <div class="col-3">
         <EntryList/>
       </div>
@@ -15,7 +24,7 @@
 
 <script>
 import { defineAsyncComponent } from 'vue'
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 
 export default{
   name: 'DaybookLayouts',
@@ -25,6 +34,9 @@ export default{
   },
   methods:{
    ...mapActions('journal',['loadingEntries'])
+  },
+  computed: {
+    ...mapState('journal',['isLoading'])
   },
   created(){
     this.loadingEntries()
